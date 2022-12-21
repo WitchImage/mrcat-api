42c00bc7ac3c6f83b2d0c3c225d8400fb3d83571
Simón Felipe Muñoz Osorio <51033434+WitchImage@users.noreply.github.com>
2022-12-20 19:02:36 -0500

    Csv (#1)
    * Add endpoint for analyzing datasets

--- api/machine_learning/supervised_learning.py
+++ api/machine_learning/supervised_learning.py
@@ -1,9 +1,3 @@
-from django.http.response import HttpResponse, JsonResponse
-from django.core.handlers.wsgi import WSGIRequest
-from django.views.decorators.csrf import csrf_exempt
-import sklearn
-import tensorflow
-import pandas as pd
 import graphviz
 import numpy as np
 from sys import argv
@@ -16,8 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_boston, load_breast_cancer, load_iris, load_wine
-import json
-# import pandas as pd
+import pandas as pd
 
 def analyze(dataset: pd.DataFrame, algorithm: str):
     if (algorithm == 'kmeans'):
@@ -47,19 +40,18 @@ def kmeans(dataset: Any, k: int):
     kmeans = KMeans(n_clusters=k, random_state=0).fit(dataset)
     kmeans.labels_
 
-    # cluster_indices = kmeans.predict([[0, 0], [12, 3]])
+    cluster_indices = kmeans.predict([[0, 0], [12, 3]])
     return kmeans.cluster_centers_
-    # print("Cluster centers:", kmeans.cluster_centers_)
+    print("Cluster centers:", kmeans.cluster_centers_)
 
-def nbayes(dataset: pd.DataFrame, class_col: str):
-    X, y = dataset.drop(class_col, axis=1), dataset.iloc[:, -1:]
-    print(X)
-    print(y)
+def nbayes(dataset: Any):
+    X, y = dataset.data, dataset.target
+    X_test: Any
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
     gnb = GaussianNB()
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
-    return f"Accuracy: {gnb.score(X_test, y_test)}"
-    return ("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+    return ("Number of mislabeled points out of a total %d points : %d"
+        % (X_test.shape[0], (y_test != y_pred).sum()))
     print("Accuracy:", gnb.score(X_test, y_test))
 
 def knn(dataset: Any, k: int, distance: Literal["manhattan", "euclidean"] = "euclidean"):
@@ -75,8 +67,8 @@ def svm(dataset: Any):
     clf = SVC()
     clf.fit(X, y)
     print(clf.predict([[1, 1]]))
-    return f"support vectors: {clf.support_vectors_}"
-    # print("number of supports vectors for each class: ", clf.n_support_)
+    return ("support vectors: ", clf.support_vectors_)
+    print("number of supports vectors for each class: ", clf.n_support_)
 
 def load_dataset(dataset: Literal["boston", "iris", "cancer", "wine"]):
     datasets = {
@@ -88,27 +80,9 @@ def load_dataset(dataset: Literal["boston", "iris", "cancer", "wine"]):
     # return datasets[dataset](return_X_y=True)
     return datasets[dataset]()
 
-def call_method(method: str, dataset: pd.DataFrame, class_col: str):
-    dataset.reset_index(drop=True, inplace=True)
+def call_method(method: str, dataset: pd.DataFrame):
     if method == "id3": return id3(dataset)
-    elif method == "kmeans": return kmeans(dataset, 2)
-    elif method == "nbayes": return nbayes(dataset, class_col)
-    elif method == "knn": return knn(dataset, 2)
+    elif method == "kmeans": return kmeans(dataset)
+    elif method == "nbayes": return nbayes(dataset)
+    elif method == "knn": return knn(dataset)
     elif method == "svm": return svm(dataset)
-
-def index(request):
-    versions = f'sklearn version: {sklearn.__version__} - tensorflow version: {tensorflow.__version__}'
-    return HttpResponse(versions)
-
-@csrf_exempt
-def analyze(request: WSGIRequest) -> JsonResponse:
-    class_col, algorithm = request.POST.get('class_col', ''), request.POST.get('algorithm', '')
-    file = request.FILES['file']
-    df = pd.read_csv(file)
-    print(f'Class: {class_col} - Algorithm: {algorithm}')
-    print(df.head())
-    res = call_method(algorithm, df, class_col)
-    print('Response', res)
-
-    response = json.loads(res)
-    return JsonResponse(response, status=200, safe=False)
